d4c88fb11d69eb0407384ae6fd49f04b44d13698
cx <1253157617@qq.com>
2017-10-04 16:20:22 +0800

    add: loading

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import App from './App.vue'
+import VueLazyload from 'vue-lazyload'
 
 import route from './router/index.js'
 Vue.use(Router);
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'src/static/loading-svg/loading-bubbles.svg',
+  attempt: 1
+})
 const router = new Router(route);
 new Vue({
   router,
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -15,18 +15,18 @@
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
             <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
               <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
-                <dd>
-                  <a href="javascript:void(0)">0 - 100</a>
+                <dd><a href="javascript:void(0)" @click="priceChecked = 'All'" :class="{'cur':priceChecked=='All'}">All</a></dd>
+                <dd v-for="(price,index) in priceFilter">
+                  <a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{'cur':priceChecked == index}">{{price.startPrice}} - {{price.endPrice}}</a>
                 </dd>
-                <dd>
+                <!-- <dd>
                   <a href="javascript:void(0)">100 - 500</a>
                 </dd>
                 <dd>
@@ -34,7 +34,7 @@
                 </dd>
                 <dd>
                   <a href="javascript:void(0)">1000 - 2000</a>
-                </dd>
+                </dd> -->
               </dl>
             </div>
 
@@ -42,20 +42,19 @@
             <div class="accessory-list-wrap">
               <div class="accessory-list col-4">
                 <ul>
-                  <li>
+                  <li v-for="(item,index) in goodsList">
                     <div class="pic">
-                      <a href="#"><img src="../static/1.jpg" alt=""></a>
+                      <a href="#"><img v-lazy="'src/static/'+item.productImg" alt=""></a>
                     </div>
                     <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">999</div>
+                      <div class="name">{{item.productName}}</div>
+                      <div class="price">{{item.productPrice}}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
                   </li>
-                  <li>
-                    <!-- fsdklfjsdlkfjsddsd -->
+                  <!-- <li>
                     <div class="pic">
                       <a href="#"><img src="../static/2.jpg" alt=""></a>
                     </div>
@@ -90,13 +89,14 @@
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
-                  </li>
+                  </li> -->
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLay" @click="closePop"></div>
       <nav-footer>
         <span slot="abc">123</span>
       </nav-footer>
@@ -106,13 +106,23 @@
 import '../assets/css/base.css'
 import '../assets/css/product.css'
 import '../assets/css/login.css'
+import '../assets/css/checkout.css'
 import axios from 'axios'
 import NavHeader from '../components/Header.vue'
 import NavFooter from '../components/Footer.vue'
     export default{
         data(){
             return {
-
+              goodsList: [],
+              priceFilter: [
+                {startPrice: 0,endPrice: 100},
+                {startPrice: 100,endPrice: 500},
+                {startPrice: 500,endPrice: 1000},
+                {startPrice: 1000,endPrice: 2000}
+              ],
+              priceChecked:'-1',
+              filterBy:false,
+              overLay:false
             }
         },
         components:{
@@ -122,13 +132,24 @@ import NavFooter from '../components/Footer.vue'
         methods:{
           getGoodsList(){
             axios.get('./../../datajson/goods.json').then(function(res){
-              console.log(res);
-            })
+              this.goodsList = res.data.result;
+            }.bind(this))
+          },
+          showFilterPop(){
+            this.filterBy = true;
+            this.overLay = true;
+          },
+          closePop(){
+            this.filterBy = false;
+            this.overLay = false;
+          },
+          setPriceFilter(index){
+            this.priceChecked = index;
+            this.closePop();
           }
         },
         mounted:function(){
-          console.log(axios)
-          this.getGoodsList()
+          this.getGoodsList();
         }
     }
 </script>
